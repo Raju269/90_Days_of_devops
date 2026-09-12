1b7793f21576e457ed27b6f1cd5199feffbe168c
Raju kumar <raju.kmr.rudra@gmail.com>
2026-09-12 16:45:44 +0000

    added help comments for backups script

--- backup.sh
+++ backup.sh
@@ -1,1 +1,22 @@
-hello ji 
+#!/bin/bash
+
+<<help 
+This is a shell script to take backups
+can also  be used with cron
+help
+
+
+source_dir="/home/ubuntu/devops-zero-to-hero/scripts"
+destination_dir="/home/ubuntu/devops-zero-to-hero/backups"
+
+function create_backup {
+	timestamp=$(date '+%Y-%m-%d-%H-%M-%S')
+
+	backup_dir="${destination_dir}/backup_${timestamp}"
+	zip -r "${backup_dir}.zip" "$source_dir"
+
+	echo "Backup completed"
+
+}
+
+create_backup $source_dir $destination_dir 
